28a31e2a350a7d0be718e25da6d2bf3daab636af
Yuhui Zhang <zyh.thu@gmail.com>
2022-07-15 12:52:22 -0700

    add isort

--- src/test_main.py
+++ src/test_main.py
@@ -1,13 +1,11 @@
 import click
 
-import typing
 
 @click.command()
 @click.option("--name", prompt="Your name", help="The person to greet.")
 def hello(name: str) -> int:
     print(f"hello {name}!")
     return 0
-
 
 
 def inc(x: int) -> int:
