2fc785b25b624450bac7506aa9df02d454796dad
Yuhui Zhang <zyh.thu@gmail.com>
2022-07-15 12:24:08 -0700

    test commit

--- src/test_main.py
+++ src/test_main.py
@@ -3,18 +3,19 @@ import click
 
 @click.command()
 @click.option("--name", prompt="Your name", help="The person to greet.")
-def hello(name):
+def hello(name: str) -> int:
     print(f"hello {name}!")
     return 0
 
 
-def inc(x):
+def inc(x: int) -> int:
     return x + 1
 
 
-def test_answer():
+def test_answer() -> None:
     assert inc(4) == 5
 
 
 if __name__ == "__main__":
     hello()
+    inc("a")  # type: ignore
