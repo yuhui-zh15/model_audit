b0c91cb6d3944b05bbb1757aac406fa2f98ca803
Yuhui Zhang <zyh.thu@gmail.com>
2022-09-12 12:43:12 -0700

    update main.py to support text dataset

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import clip  # type: ignore
 import torch
 
-from datasets import AttributeDataset, create_dataloader
+from datasets import AttributeDataset, TextDataset, create_dataloader
 from models import Linear
 from trainer import run_one_epoch
 
@@ -16,7 +16,17 @@ def main():
         filter_func=lambda x: x["attributes"]["split"] == "val",
         label_func=lambda x: int(x["attributes"]["Male"] == 1),
     )
-    dataloader = create_dataloader(dataset=dataset, transform=transform)
+    dataloader = create_dataloader(
+        dataset=dataset, modality="image", transform=transform
+    )
+
+    text_dataset = TextDataset(
+        data=[
+            {"text": "a photo of a man.", "label": 1},
+            {"text": "a photo of a woman.", "label": 0},
+        ],
+    )
+    text_dataloader = create_dataloader(dataset=text_dataset, modality="text")
     opt = torch.optim.Adam(model.parameters(), lr=1e-3)
 
     for epoch_idx in range(10):
@@ -41,7 +51,21 @@ def main():
             eval=True,
             verbose=True,
         )
+
         print(f"{metrics['acc']=}, {metrics['loss']=}")
+
+        text_metrics = run_one_epoch(
+            dataloader=text_dataloader,
+            model=model,
+            clip_model=clip_model,
+            modality="text",
+            opt=None,
+            epoch_idx=epoch_idx,
+            eval=True,
+            verbose=True,
+        )
+
+        print(f"{text_metrics['acc']=}, {text_metrics['loss']=}")
 
 
 if __name__ == "__main__":
