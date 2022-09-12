613b4f6b24d7fb4e527021cf41b63863ee814ad3
Yuhui Zhang <zyh.thu@gmail.com>
2022-09-12 16:35:15 -0700

    support fairface

--- src/test_all.py
+++ src/test_all.py
@@ -4,6 +4,7 @@ from datasets import AttributeDataset, ImageDataset, TextDataset, create_dataloa
 
 CELEBA_PATH = "/pasteur/u/yuhuiz/data/CelebA/processed_attribute_dataset"
 WATERBIRDS_PATH = "/pasteur/u/yuhuiz/data/Waterbird/processed_attribute_dataset"
+FAIRFACE_PATH = "/pasteur/u/yuhuiz/data/FairFace/processed_attribute_dataset"
 
 
 def test_image_dataset():
@@ -20,25 +21,39 @@ def test_image_dataset():
     assert dataloader is not None
 
 
-def test_attribute_dataset_celeba():
+def test_attribute_dataset_waterbirds():
     dataset = AttributeDataset(
         path=WATERBIRDS_PATH,
         filter_func=lambda x: x["attributes"]["split"] == "val",
         label_func=lambda x: x["attributes"]["waterbird"],
     )
+    assert len(dataset) == 1199
     dataloader = create_dataloader(
         dataset=dataset, modality="image", transform=lambda x: torch.zeros(100)
     )
     assert dataloader is not None
 
 
-def test_attribute_dataset_waterbirds():
+def test_attribute_dataset_celeba():
     dataset = AttributeDataset(
         path=CELEBA_PATH,
         filter_func=lambda x: x["attributes"]["split"] == "val",
         label_func=lambda x: int(x["attributes"]["Male"] == 1),
     )
     assert len(dataset) == 19867
+    dataloader = create_dataloader(
+        dataset=dataset, modality="image", transform=lambda x: torch.zeros(100)
+    )
+    assert dataloader is not None
+
+
+def test_attribute_dataset_fairface():
+    dataset = AttributeDataset(
+        path=FAIRFACE_PATH,
+        filter_func=lambda x: x["attributes"]["split"] == "val",
+        label_func=lambda x: int(x["attributes"]["gender"] == "Male"),
+    )
+    assert len(dataset) == 10954
     dataloader = create_dataloader(
         dataset=dataset, modality="image", transform=lambda x: torch.zeros(100)
     )
@@ -61,4 +76,5 @@ if __name__ == "__main__":
     test_image_dataset()
     test_attribute_dataset_celeba()
     test_attribute_dataset_waterbirds()
+    test_attribute_dataset_fairface()
     test_text_dataset()
