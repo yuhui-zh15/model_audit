5b357c1eea96f746ec09be680e01a3170b0c062f
Yuhui Zhang <zyh.thu@gmail.com>
2022-10-17 20:37:35 -0700

    refactor dro code

--- src/train_models.py
+++ src/train_models.py
@@ -10,8 +10,6 @@ from torch.utils.data import DataLoader, TensorDataset
 from models import Linear
 from utils import LossComputer, computing_subgroup_metrics, subgrouping
 
-CLIP_MODEL = "ViT-B/32"
-
 
 def train_one_epoch(
     dataloader: torch.utils.data.DataLoader,
@@ -138,8 +136,84 @@ def train_image_model(
     return model
 
 
+def train_image_model_dro(
+    features: torch.Tensor,
+    labels: torch.Tensor,
+    group_idxs: torch.Tensor,
+    train_idxs: list,
+    val_idxs: list,
+    data: Optional[list] = None,
+    fields: Optional[list] = None,
+    n_epochs: int = 25,
+    batch_size: int = 32,
+    lr: float = 1e-3,
+) -> torch.nn.Module:
+    assert len(features) == len(
+        labels
+    ), "Features and labels should have the same length."
+    features = F.normalize(features)
+
+    train_features = features[train_idxs]
+    train_labels = labels[train_idxs]
+    train_group_idxs = group_idxs[train_idxs]
+    val_features = features[val_idxs]
+    val_labels = labels[val_idxs]
+
+    train_dataset = TensorDataset(train_features, train_labels, train_group_idxs)
+    val_dataset = TensorDataset(val_features, val_labels)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+    d_model = features.shape[1]
+    n_classes = int(labels.max().item() + 1)
+    model = Linear(d_model, n_classes).cuda()
+    opt = torch.optim.Adam(model.parameters(), lr=lr)
+
+    group_counts = train_group_idxs.bincount().cuda()
+
+    loss_dro = LossComputer(
+        torch.nn.CrossEntropyLoss(reduce=False),
+        True,
+        0.2,
+        0.1,
+        None,
+        0,
+        0.01,
+        False,
+        False,
+        len(group_counts),
+        group_counts,
+        [f"g{i}" for i in range(len(group_counts))],
+    )
+
+    for epoch_idx in range(n_epochs):
+        train_one_epoch_dro(train_dataloader, model, opt, loss_dro)
+        metrics = evaluate(val_dataloader, model)
+        print(epoch_idx, metrics)
+
+        if fields is not None:
+            assert (
+                data is not None
+            ), "Data must be provided to compute subgroup metrics."
+            assert len(data) == len(
+                features
+            ), "Data and features should have the same length."
+            preds, labels = metrics["preds"], metrics["labels"]
+            subgroups = subgrouping([data[idx] for idx in val_idxs], fields)
+            subgroup_metrics = computing_subgroup_metrics(
+                preds.tolist(), labels.tolist(), subgroups
+            )
+            print(sorted(subgroup_metrics.items(), key=lambda x: x[1]))
+
+    return model
+
+
 def train_image_model_waterbird(
-    data_path: str, feature_path: str, close_gap: bool = False, coco_norm: bool = True
+    data_path: str,
+    feature_path: str,
+    close_gap: bool = False,
+    coco_norm: bool = True,
+    dro: bool = False,
 ):
     data = [json.loads(line) for line in open(data_path)]
     features = F.normalize(torch.load(feature_path))
@@ -157,20 +231,50 @@ def train_image_model_waterbird(
         coco_features = torch.load("pytorch_cache/features/coco_features_vitb32.pt")
         global_mean = F.normalize(coco_features["image_features"]).mean(0)
 
-    model = train_image_model(
-        features,
-        labels,
-        train_idxs,
-        val_idxs,
-        data=data,
-        fields=["waterbird", "waterplace"],
-        close_gap=close_gap,
-        global_mean=global_mean,
+    if not dro:
+        model = train_image_model(
+            features,
+            labels,
+            train_idxs,
+            val_idxs,
+            data=data,
+            fields=["waterbird", "waterplace"],
+            close_gap=close_gap,
+            global_mean=global_mean,
+        )
+    else:
+        assert not close_gap, "DRO is not compatible with close gap."
+        group2idx = {
+            (0, 0): 0,
+            (0, 1): 1,
+            (1, 0): 2,
+            (1, 1): 3,
+        }
+        group_idxs = torch.tensor(
+            [
+                group2idx.get(
+                    (item["attributes"]["waterbird"], item["attributes"]["waterplace"]),
+                    -1,
+                )
+                for item in data
+            ]
+        )
+        model = train_image_model_dro(
+            features,
+            labels,
+            group_idxs,
+            train_idxs,
+            val_idxs,
+            data=data,
+            fields=["waterbird", "waterplace"],
+        )
+
+    torch.save(
+        model.state_dict(), f"waterbird_linear_model_gap{not close_gap}_dro{dro}.pt"
     )
-    torch.save(model.state_dict(), f"waterbird_linear_model_gap{not close_gap}.pt")
 
 
-def train_image_model_fairface(data_path: str, feature_path: str):
+def train_image_model_fairface(data_path: str, feature_path: str, dro: bool = False):
     data = [json.loads(line) for line in open(data_path)]
     features = F.normalize(torch.load(feature_path))
     labels = torch.tensor(
@@ -202,10 +306,31 @@ def train_image_model_fairface(data_path: str, feature_path: str):
             if random.random() <= montana_race_norm[race]:
                 sampled_train_idxs.append(idx)
 
-    model = train_image_model(
-        features, labels, sampled_train_idxs, val_idxs, data=data, fields=["race"]
-    )
-    torch.save(model.state_dict(), "fairface_linear_model.pt")
+    if not dro:
+        model = train_image_model(
+            features, labels, sampled_train_idxs, val_idxs, data=data, fields=["race"]
+        )
+    else:
+        group2idx = {
+            "White": 0,
+            "Indian": 1,
+            "East Asian": 2,
+            "Black": 3,
+        }
+        group_idxs = torch.tensor(
+            [group2idx.get(item["attributes"]["race"], -1) for item in data]
+        )
+        model = train_image_model_dro(
+            features,
+            labels,
+            group_idxs,
+            sampled_train_idxs,
+            val_idxs,
+            data=data,
+            fields=["race"],
+        )
+
+    torch.save(model.state_dict(), f"fairface_linear_model_dro{dro}.pt")
 
 
 def train_image_model_dspites(data_path: str, feature_path: str):
@@ -235,201 +360,10 @@ def train_image_model_dspites(data_path: str, feature_path: str):
     torch.save(model.state_dict(), "dspites_linear_model.pt")
 
 
-def train_image_model_waterbird_dro(data_path: str, feature_path: str):
-    data = [json.loads(line) for line in open(data_path)]
-    for item in data:
-        if (
-            item["attributes"]["waterbird"] == 1
-            and item["attributes"]["waterplace"] == 1
-        ):
-            item["attributes"]["group_idx"] = 0
-        elif (
-            item["attributes"]["waterbird"] == 1
-            and item["attributes"]["waterplace"] == 0
-        ):
-            item["attributes"]["group_idx"] = 1
-        elif (
-            item["attributes"]["waterbird"] == 0
-            and item["attributes"]["waterplace"] == 1
-        ):
-            item["attributes"]["group_idx"] = 2
-        else:
-            item["attributes"]["group_idx"] = 3
-
-    labels = torch.tensor([item["attributes"]["waterbird"] for item in data])
-
-    train_idxs = [
-        i for i, item in enumerate(data) if item["attributes"]["split"] == "train"
-    ]
-    val_idxs = [
-        i for i, item in enumerate(data) if item["attributes"]["split"] == "val"
-    ]
-
-    features = F.normalize(torch.load(feature_path))
-
-    train_features = features[train_idxs]
-    train_labels = labels[train_idxs]
-    train_group_idxs = torch.tensor([item["attributes"]["group_idx"] for item in data])[
-        train_idxs
-    ]
-    val_features = features[val_idxs]
-    val_labels = labels[val_idxs]
-
-    train_dataset = TensorDataset(train_features, train_labels, train_group_idxs)
-    val_dataset = TensorDataset(val_features, val_labels)
-    train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-    val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False)
-
-    model = Linear(512, 2).cuda()
-    opt = torch.optim.Adam(model.parameters(), lr=1e-3)
-
-    group_counts = torch.tensor(
-        [
-            np.sum(
-                [
-                    item["attributes"]["group_idx"] == i
-                    for item in np.array(data)[train_idxs]
-                ]
-            )
-            for i in range(4)
-        ]
-    ).cuda()
-    print(group_counts)
-    loss_dro = LossComputer(
-        torch.nn.CrossEntropyLoss(reduce=False),
-        True,
-        0.2,
-        0.1,
-        np.array([0.0, 0.0, 0.0, 0.0]),
-        0,
-        0.01,
-        False,
-        False,
-        4,
-        group_counts,
-        ["ww", "wl", "lw", "ll"],
-    )
-    for epoch_idx in range(25):
-        train_one_epoch_dro(train_dataloader, model, opt, loss_dro)
-        metrics = evaluate(val_dataloader, model)
-        print(epoch_idx, metrics)
-
-        preds, labels = metrics["preds"], metrics["labels"]
-        subgroups = subgrouping(
-            list(np.array(data)[val_idxs]), ["waterbird", "waterplace"]
-        )
-        subgroup_metrics = computing_subgroup_metrics(preds, labels, subgroups)  # type: ignore
-        print(subgroup_metrics)
-
-    torch.save(model.state_dict(), "waterbird_linear_model_dro.pt")
-
-
-def train_image_model_fairface_dro(data_path: str, feature_path: str):
-    data = [json.loads(line) for line in open(data_path)]
-    labels = torch.tensor(
-        [1 if item["attributes"]["gender"] == "Female" else 0 for item in data]
-    )
-
-    train_idxs = [
-        i for i, item in enumerate(data) if item["attributes"]["split"] == "train"
-    ]
-    val_idxs = [
-        i for i, item in enumerate(data) if item["attributes"]["split"] == "val"
-    ]
-
-    montana_race = {
-        "White": 90.6 / 100,
-        "Indian": 6.2 / 100,
-        "East Asian": 0.5 / 100,
-        "Black": 0.3 / 100,
-    }
-    race2idx = {
-        "White": 0,
-        "Indian": 1,
-        "East Asian": 2,
-        "Black": 3,
-    }
-    for item in data:
-        race = item["attributes"]["race"]
-        item["attributes"]["group_idx"] = race2idx[race] if race in race2idx else -1
-
-    montana_race_norm = {
-        key: montana_race[key] / montana_race["White"] for key in montana_race
-    }
-    print(montana_race_norm)
-
-    sampled_train_idxs = []
-    random.seed(1234)
-    for idx in train_idxs:
-        race = data[idx]["attributes"]["race"]
-        if race in montana_race_norm:
-            if random.random() <= montana_race_norm[race]:
-                sampled_train_idxs.append(idx)
-
-    # race_distribution = Counter([item["attributes"]["race"] for item in sampled_train_data]).most_common()
-
-    features = F.normalize(torch.load(feature_path))
-
-    train_features = features[sampled_train_idxs]
-    train_labels = labels[sampled_train_idxs]
-    train_group_idxs = torch.tensor([item["attributes"]["group_idx"] for item in data])[
-        sampled_train_idxs
-    ]
-    val_features = features[val_idxs]
-    val_labels = labels[val_idxs]
-
-    train_dataset = TensorDataset(train_features, train_labels, train_group_idxs)
-    val_dataset = TensorDataset(val_features, val_labels)
-    train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-    val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False)
-
-    model = Linear(512, 2).cuda()
-    opt = torch.optim.Adam(model.parameters(), lr=1e-3)
-
-    group_counts = torch.tensor(
-        [
-            np.sum(
-                [
-                    item["attributes"]["group_idx"] == i
-                    for item in np.array(data)[train_idxs]
-                ]
-            )
-            for i in range(4)
-        ]
-    ).cuda()
-    print(group_counts)
-    loss_dro = LossComputer(
-        torch.nn.CrossEntropyLoss(reduce=False),
-        True,
-        0.2,
-        0.1,
-        np.array([0.0, 0.0, 0.0, 0.0]),
-        0,
-        0.01,
-        False,
-        False,
-        4,
-        group_counts,
-        ["ww", "wl", "lw", "ll"],
-    )
-    for epoch_idx in range(25):
-        train_one_epoch_dro(train_dataloader, model, opt, loss_dro)
-        metrics = evaluate(val_dataloader, model)
-        print(epoch_idx, metrics)
-
-        preds, labels = metrics["preds"], metrics["labels"]
-        subgroups = subgrouping(list(np.array(data)[val_idxs]), ["race"])
-        subgroup_metrics = computing_subgroup_metrics(preds, labels, subgroups)  # type: ignore
-        print(subgroup_metrics)
-
-    torch.save(model.state_dict(), "fairface_linear_model_dro.pt")
-
-
 if __name__ == "__main__":
     train_image_model_waterbird(
         "../data/Waterbird/processed_attribute_dataset/attributes.jsonl",
         "pytorch_cache/features/waterbird_features_vitb32.pt",
-        close_gap=False,
     )
     train_image_model_fairface(
         "../data/FairFace/processed_attribute_dataset/attributes.jsonl",
@@ -439,11 +373,13 @@ if __name__ == "__main__":
         "../data/TriangleSquare/processed_attribute_dataset/attributes.jsonl",
         "pytorch_cache/features/trianglesquare_features_vitb32.pt",
     )
-    # train_image_model_waterbird_dro(
-    #     "../data/Waterbird/processed_attribute_dataset/attributes.jsonl",
-    #     "pytorch_cache/features/waterbird_features_vitb32.pt",
-    # )
-    # train_image_model_fairface_dro(
-    #     "../data/FairFace/processed_attribute_dataset/attributes.jsonl",
-    #     "pytorch_cache/features/fairface_features_vitb32.pt",
-    # )
+    train_image_model_waterbird(
+        "../data/Waterbird/processed_attribute_dataset/attributes.jsonl",
+        "pytorch_cache/features/waterbird_features_vitb32.pt",
+        dro=True,
+    )
+    train_image_model_fairface(
+        "../data/FairFace/processed_attribute_dataset/attributes.jsonl",
+        "pytorch_cache/features/fairface_features_vitb32.pt",
+        dro=True,
+    )
