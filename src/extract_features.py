3fb962001ac8fe29d8801a281e1a82cc14ea982c
Yuhui Zhang <zyh.thu@gmail.com>
2022-10-19 14:38:32 -0700

    add imagenet features

--- src/extract_features.py
+++ src/extract_features.py
@@ -1,10 +1,13 @@
 import json
+import os
 
 import clip  # type: ignore
 import torch
+from torchvision.datasets import ImageNet  # type: ignore
 
 from datasets import ImageDataset, TextDataset, create_dataloader
 from trainer import extract_features
+from utils import openai_imagenet_classes, openai_imagenet_template
 
 
 def filter_name(name: str) -> str:
@@ -61,7 +64,57 @@ def extract_features_coco(model_name: str):
 
 
 def extract_features_imagenet(model_name: str):
-    raise NotImplementedError
+    clip_model, transform = clip.load(name=model_name, device="cuda")
+    clip_model = clip_model.float()
+
+    image_data = [
+        {"image": item[0], "label": item[1]}
+        for item in ImageNet(root=os.path.expanduser("~/.cache"), split="val").imgs
+    ]
+    image_dataset = ImageDataset(image_data)
+    image_dataloader = create_dataloader(
+        dataset=image_dataset,
+        modality="image",
+        transform=transform,
+        shuffle=False,
+        batch_size=1024,
+        num_workers=16,
+    )
+    image_features = extract_features(
+        dataloader=image_dataloader,
+        clip_model=clip_model,
+        modality="image",
+        verbose=True,
+    )
+    image_labels = torch.tensor([item["label"] for item in image_data])
+
+    text_data = [
+        {"text": prompt(classname), "label": label}
+        for label, classname in enumerate(openai_imagenet_classes)
+        for prompt in openai_imagenet_template
+    ]
+    text_dataset = TextDataset(text_data)
+    text_dataloader = create_dataloader(
+        dataset=text_dataset,
+        modality="text",
+        shuffle=False,
+        batch_size=1024,
+        num_workers=16,
+    )
+    text_features = extract_features(
+        dataloader=text_dataloader, clip_model=clip_model, modality="text", verbose=True
+    )
+    text_labels = torch.tensor([item["label"] for item in text_data])
+
+    torch.save(
+        {
+            "image_features": image_features,
+            "text_features": text_features,
+            "image_labels": image_labels,
+            "text_labels": text_labels,
+        },
+        f"imagenet_features_{filter_name(model_name)}.pt",
+    )
 
 
 def extract_features_others(model_name: str, dataset: str):
@@ -100,8 +153,8 @@ def extract_features_others(model_name: str, dataset: str):
 
 
 if __name__ == "__main__":
-    extract_features_coco(model_name="ViT-B/32")
+    # extract_features_coco(model_name="ViT-B/32")
     extract_features_imagenet(model_name="ViT-B/32")
-    extract_features_others(model_name="ViT-B/32", dataset="Waterbird")
-    extract_features_others(model_name="ViT-B/32", dataset="FairFace")
-    extract_features_others(model_name="ViT-B/32", dataset="TriangleSquare")
+    # extract_features_others(model_name="ViT-B/32", dataset="Waterbird")
+    # extract_features_others(model_name="ViT-B/32", dataset="FairFace")
+    # extract_features_others(model_name="ViT-B/32", dataset="TriangleSquare")
