2d2911423f820e27cffa60e8d6b4a82d6f874c5b
Yuhui Zhang <zyh.thu@gmail.com>
2022-10-19 12:35:47 -0700

    add transfer imagenet

--- src/transfer/train_coco.sh
+++
@@ -1,9 +0,0 @@
-python transfer/train.py adam original bias img
-python transfer/train.py adam original bias txt
-python transfer/train.py adam original mlp img
-python transfer/train.py adam original mlp txt
-
-python transfer/train.py adam centering_norenorm bias img  
-python transfer/train.py adam centering_norenorm bias txt
-python transfer/train.py adam centering_norenorm mlp img  
-python transfer/train.py adam centering_norenorm mlp txt
