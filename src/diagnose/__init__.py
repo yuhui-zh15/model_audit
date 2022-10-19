edc60abffab549f46a1856b58910beaad1b28867
Yuhui Zhang <zyh.thu@gmail.com>
2022-10-19 10:57:20 -0700

    finish refactor correlation analysis

---
+++ src/diagnose/__init__.py
@@ -0,0 +1,7 @@
+from .diagnose_utils import compute_correlation  # noqa
+from .diagnose_utils import compute_dataset_correlation  # noqa
+from .diagnose_utils import compute_subgroup_correlation  # noqa
+from .diagnose_utils import get_model_output  # noqa
+from .prepare_text_datasets import prepare_dspites  # noqa
+from .prepare_text_datasets import prepare_fairface  # noqa
+from .prepare_text_datasets import prepare_waterbird  # noqa
